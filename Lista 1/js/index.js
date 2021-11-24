be7c62b2330c47ef3314505d91fa21d36a04cc01
Bartomiej Zocki <b.zlocki@bonasoft.pl>
2021-11-24 12:25:17 +0100

    Lista 6, p. 1 i 3

--- Lista 1/js/index.js
+++ Lista 1/js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var name = window.prompt("Podaj swoje imię!: ");
-    var span = document.getElementById("greeting");
-    var age = parseInt(window.prompt("Podaj swój wiek: "));
-    var height = parseFloat(window.prompt("Podaj swój wzrost w metrach: "));
+    // var name = window.prompt("Podaj swoje imię!: ");
+    // var span = document.getElementById("greeting");
+    // var age = parseInt(window.prompt("Podaj swój wiek: "));
+    // var height = parseFloat(window.prompt("Podaj swój wzrost w metrach: "));
     if (name && span && age) {
         span.innerHTML = "Witaj na naszej stronie " + name + " w wieku " + age + " o wzroście " + height + "m";
     }
